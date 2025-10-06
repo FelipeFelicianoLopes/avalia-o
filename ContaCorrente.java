a1666cabf8505ac40cbde40b1e7159d3cbbc610d
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2025-10-06 00:13:02 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -18,38 +18,4 @@ public class ContaCorrente extends Conta {
         super.mostrarDados();
     }
 
-    public class ContaInvestimento extends Conta {
-        private double taxaAdministracao;
-
-        public double getTaxaAdministracao() {
-            return taxaAdministracao;
-        }
-
-        public void setTaxaAdministracao(double taxaAdministracao) {
-            this.taxaAdministracao = taxaAdministracao;
-        }
-
-        public ContaInvestimento(String numero, String titular, double saldo, double taxaAdministracao) {
-            super(numero, titular, saldo);
-            this.taxaAdministracao = taxaAdministracao;
-        }
-
-        @Override
-        public void mostrarDados() {
-            super.mostrarDados();
-            System.out.println("Taxa de administração: " + taxaAdministracao + "%");
-        }
-
-        public void mostrarDados(boolean considerarTaxa) {
-            super.mostrarDados();
-            if (considerarTaxa) {
-                double saldoLiquido = getSaldo() * (1 - (taxaAdministracao / 100)); // Saldo após taxa
-                System.out.println("Saldo líquido (após taxa de administração): " + saldoLiquido);
-            }
-        }
-    }
-
-
-
-
-}
+  
